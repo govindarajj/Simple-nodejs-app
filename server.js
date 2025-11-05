ce0e6f4299a4260b40102b5d87a4ad7cc2c23d31
govindarajj <106022701+govindarajj@users.noreply.github.com>
2025-11-05 17:54:11 +0530

    three rocket emojis 🚀🚀🚀"

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, World! This Node.js app is running inside Kubernetes 🚀");
+  res.send("Hello, World! This Node.js app is running inside Kubernetes 🚀🚀🚀");
 });
 
 app.listen(port, () => {
