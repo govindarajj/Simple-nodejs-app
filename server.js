b2d878dc601a5ba20f9f80b1df84b72cc59a5671
govindarajj <106022701+govindarajj@users.noreply.github.com>
2025-11-05 14:42:17 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World! This Node.js app is running inside Docker 🚀");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
